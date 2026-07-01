b85da9bea7236b83f04809b122acb119737843b9
isahlopesss <isadorafl05@gmail.com>
2026-07-01 16:42:04 -0400

    fix: mitigação de vulnerabilidades OWASP e atualização de dependências

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,8 +42,10 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => $request->role,
         ]);
+        
+        $user->role = $request->role;
+        $user->save();
 
         event(new Registered($user));
 
