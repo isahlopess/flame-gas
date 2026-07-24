d84657ff49a4192e4cb3bccad00c73716e090f68
isahlopesss <isadorafl05@gmail.com>
2026-07-24 09:45:27 -0400

    fix: corrige permissao de leitura da vercel (tmp)

--- api/index.php
+++ api/index.php
@@ -1,3 +1,29 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$storagePath = '/tmp/storage';
+$app->useStoragePath($storagePath);
+
+$directories = [
+    $storagePath . '/app/public',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/framework/testing',
+    $storagePath . '/framework/views',
+    $storagePath . '/logs',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+$app->handleRequest(Request::capture());
